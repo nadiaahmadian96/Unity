fd8731cc19da2dda43e33bef6aa333f78166caa9
Nadia Ahmadian <nadia.ahmadian96@yahoo.com>
2025-01-20 21:24:17 +0330

    Create PausetheGame.cs

---
+++ PausetheGame.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour
+{
+    private bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
+    }
+
+    void TogglePause()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = isPaused ? 0 : 1;
+    }
+}
